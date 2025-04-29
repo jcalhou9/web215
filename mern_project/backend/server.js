7f671e50f0bc9f5b322510c384a54cf931de63bb
jcalhou9 <jcalhou9@email.cpcc.edu>
2025-04-29 12:14:21 -0400

    update cors
    keep moving, you see nothing

--- mern_project/backend/server.js
+++ mern_project/backend/server.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 //allowed frontend origins
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://https://assignmentmanager-8mn3.onrender.com'
+    'https://assignmentmanager-8mn3.onrender.com'
 ];
 
 //middleware
